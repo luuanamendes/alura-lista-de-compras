2c0922651d97f3bf2956c461105bc1d80b899313
Luana Mendes <luanamendessant@outlook.com>
2025-04-17 15:54:45 -0300

    construindo item da lista

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const inputItem = document.getElementById("input-item")
+const btnAdd = document.getElementById("adicionar-item")
 
-console.log(inputItem.value);
+btnAdd.addEventListener("click", (evento) => {
+    evento.preventDefault()
+    if(inputItem.value === ""){
+        alert("Por favor, insira um item!")
+        return
+    }
+
+    const itemLista = document.createElement("li");
+    const containerItemLista = document.createElement("div");
+    containerItemLista.classList.add("lista-item-container");
+    
+    
+    
+})
+
