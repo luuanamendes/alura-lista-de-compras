0b02dd36c37ce99124cb1119abc76caa0b81b027
Luana Mendes <luanamendessant@outlook.com>
2025-04-17 16:03:29 -0300

    construindo inputCheckbox

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const inputItem = document.getElementById("input-item")
-const btnAdd = document.getElementById("adicionar-item")
+const inputItem = document.getElementById("input-item");
+const btnAdd = document.getElementById("adicionar-item");
+let contador = 0;
 
 btnAdd.addEventListener("click", (evento) => {
-    evento.preventDefault()
+    evento.preventDefault();
     if(inputItem.value === ""){
-        alert("Por favor, insira um item!")
-        return
+        alert("Por favor, insira um item!");
+        return;
     }
 
     const itemLista = document.createElement("li");
     const containerItemLista = document.createElement("div");
     containerItemLista.classList.add("lista-item-container");
-    
+    const inputCheckbox = document.createElement("input");
+    inputCheckbox.type = "checkbox";
+    inputCheckbox.id = "checkbox-" + contador++;
+
     
     
 })
